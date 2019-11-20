5d4ede66b60ddc5bc5e33036750a8feec4271325
Robin <robin@robin-it.com>
2019-11-20 14:55:36 +0100

    Initiatives: expose mirror separately in Discourse

--- src/plugins/discourse/loadDiscourse.js
+++ src/plugins/discourse/loadDiscourse.js
@@ -3,7 +3,11 @@
 import Database from "better-sqlite3";
 import base64url from "base64url";
 import {Fetcher} from "./fetch";
-import {SqliteMirrorRepository, type ReadRepository} from "./mirrorRepository";
+import {
+  SqliteMirrorRepository,
+  type ReadRepository,
+  type MirrorRepository,
+} from "./mirrorRepository";
 import {Mirror, type MirrorOptions} from "./mirror";
 import {createGraph} from "./createGraph";
 import {TaskReporter} from "../../util/taskReporter";
@@ -20,14 +24,19 @@ export type Options = {|
   +cacheDirectory: string,
 |};
 
+export function createMirrorRepository(options: Options): MirrorRepository {
+  const {serverUrl} = options.discourseServer;
+  const filename = base64url.encode(serverUrl) + ".db";
+  const db = new Database(path.join(options.cacheDirectory, filename));
+  return new SqliteMirrorRepository(db, serverUrl);
+}
+
 export async function loadDiscourse(
   options: Options,
   reporter: TaskReporter
 ): Promise<Graph> {
   const {serverUrl, mirrorOptions} = options.discourseServer;
-  const filename = base64url.encode(serverUrl) + ".db";
-  const db = new Database(path.join(options.cacheDirectory, filename));
-  const repo = new SqliteMirrorRepository(db, serverUrl);
+  const repo = createMirrorRepository(options);
   const fetcher = new Fetcher({serverUrl});
   const mirror = new Mirror(repo, fetcher, serverUrl, mirrorOptions);
   await mirror.update(reporter);
